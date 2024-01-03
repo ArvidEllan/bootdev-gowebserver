8b0a0409113a62d51cbc14cffe18e575ba6b01e9
“ArvidEllan” <ellanwambugu6@gmail.com>
2024-01-03 17:21:58 +0300

    update on static file rendering 3.2 orij , mapping static files correctly

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # bootdev-gowebserver
+
+#practise project on building a go webserver from scratch 
--- main.go
+++ main.go
@@ -4,40 +4,38 @@ import (
 	"log"
 	"net/http"
 )
-
-func main() {
+func main()  {
+	const filepathRoot = "."
 	const port = "8080"
 
-	// Serve static files from the root directory
+
 	fs := http.FileServer(http.Dir("."))
-	http.Handle("/", http.StripPrefix("/", fs))
+	http.Handle("/",http.StripPrefix("/",fs))
+	http.Handle("/assets/",http.StripPrefix("/assets",http.FileServer(http.Dir("assets"))))
 
-	// Serve static files from the "assets" directory
-	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
+    
 
-	// Apply CORS middleware
-	handler := middlewareCors(http.DefaultServeMux)
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	corsMux := middlewareCors(mux)
 
 	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: handler,
+		Addr: ":" + port,
+		Handler: corsMux,
 	}
-
-	log.Printf("Serving on port: %s\n", port)
+	log.Printf("Serving files from %s on port : %s\n" ,filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
 }
-
-func middlewareCors(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+func middlewareCors(next http.Handler) http.Handler{
+	return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request)  {
+		w.Header().Set("Access-Control-Allow-Origins", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET,POST,OPTIONS,PUT,DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "*")
-
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
 			return
 		}
-
-		next.ServeHTTP(w, r)
+		next.ServeHTTP(w,r)
+		
 	})
 }
