5d499dd23a16dffa4f5992c2e0598135b1706e04
“ArvidEllan” <ellanwambugu6@gmail.com>
2024-01-03 17:42:05 +0300

    adding the readiness endpoint

--- main.go
+++ main.go
@@ -26,16 +26,9 @@ func main()  {
 	log.Printf("Serving files from %s on port : %s\n" ,filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
 }
-func middlewareCors(next http.Handler) http.Handler{
-	return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request)  {
-		w.Header().Set("Access-Control-Allow-Origins", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET,POST,OPTIONS,PUT,DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "*")
-		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-		next.ServeHTTP(w,r)
-		
-	})
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
