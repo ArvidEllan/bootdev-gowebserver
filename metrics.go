669934774c692e6c13bf44cbf5a219da6cab8a2c
“ArvidEllan” <ellanwambugu6@gmail.com>
2024-01-05 11:17:11 +0300

    admin metrics api update

--- metrics.go
+++ metrics.go
@@ -3,22 +3,26 @@ package main
 import (
 	"fmt"
 	"net/http"
-
-	//"golang.org/x/tools/go/cfg"
 )
 
-func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request)  {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/html")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("Hits: %d\n", cfg.fileserverHits)))
-	
+	w.Write([]byte(fmt.Sprintf(`
+<html>
+
+<body>
+	<h1>Welcome, Chirpy Admin</h1>
+	<p>Chirpy has been visited %d times!</p>
+</body>
+
+</html>
+	`, cfg.fileserverHits)))
 }
 
-func (cfg *apiConfig)middlewareMetricsInc(next http.Handler) http.Handler  {
-	return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request)  {
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		cfg.fileserverHits++
 		next.ServeHTTP(w, r)
-		
 	})
-	
 }
